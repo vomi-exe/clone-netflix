bf76c397ec5d28fb50f2f96ebb367de69bc724d0
vomi-exe <mehtavomi@gmail.com>
2022-01-26 22:44:59 +0530

    register page done frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './pages/home/Home';
+import Watch from './pages/watch/Watch';
 import "./app.scss";
+import Register from './pages/register/Register';
 
 function App() {
   return (
     <>
-      <Home />
+      <Register />
     </>
   );
 }
