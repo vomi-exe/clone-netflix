a73ef1edb93fd0118413f3754b8a715f5544fab3
vomi-exe <mehtavomi@gmail.com>
2022-01-26 21:04:32 +0530

    final card hover animation and css

--- src/components/listItem/ListItem.jsx
+++ src/components/listItem/ListItem.jsx
@@ -5,7 +5,7 @@ import "./listItem.scss";
 const ListItem = ({ index, setNum, num }) => {
 
     const [isHovered, setIsHovered] = useState(false);
-
+    const trailer = "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
 
     return (
         <div className="listItem"
@@ -18,23 +18,26 @@ const ListItem = ({ index, setNum, num }) => {
                 src="https://occ-0-1723-92.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABU7D36jL6KiLG1xI8Xg_cZK-hYQj1L8yRxbQuB0rcLCnAk8AhEK5EM83QI71bRHUm0qOYxonD88gaThgDaPu7NuUfRg.jpg?r=4ee"
                 alt=""
             />
-            <div className="itemInfo">
-                <div className="icons">
-                    <PlayArrow />
-                    <AddOutlined />
-                    <ThumbUpAltOutlined />
-                    <ThumbDownOutlined />
+            {isHovered && (<>
+                <video src={trailer} autoPlay={true} preLoad="metadata" loop />
+                <div className="itemInfo">
+                    <div className="icons">
+                        <PlayArrow className="icon" />
+                        <AddOutlined className="icon" />
+                        <ThumbUpAltOutlined className="icon" />
+                        <ThumbDownOutlined className="icon" />
+                    </div>
+                    <div className="itemInfoTop">
+                        <span>1 hour 14 min</span>
+                        <span className="limit">+16</span>
+                        <span>1999</span>
+                    </div>
+                    <div className="desc">Lorem ipsum dolor sit amet consectetur adipisicing
+                        elit. Deserunt impedit nam eveniet voluptates est minus.
+                    </div>
+                    <div className="genre">Action</div>
                 </div>
-                <div className="itemInfoTop">
-                    <span>1 hour 14 min</span>
-                    <span className="limit">+16</span>
-                    <span>1999</span>
-                </div>
-                <div className="desc">Lorem ipsum dolor sit amet consectetur adipisicing
-                    elit. Deserunt impedit nam eveniet voluptates est minus.
-                </div>
-                <div className="genre">Action</div>
-            </div>
+            </>)}
         </div>
     );
 };
