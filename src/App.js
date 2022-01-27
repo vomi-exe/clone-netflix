37a9ec6f7431bdfc30f954c7311cdb8655e8ceeb
vomi-exe <mehtavomi@gmail.com>
2022-01-27 15:11:35 +0530

    project complete

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Home from './pages/home/Home';
 import Watch from './pages/watch/Watch';
 import "./app.scss";
 import Register from './pages/register/Register';
+import Login from './pages/login/Login';
 
 function App() {
   return (
     <>
-      <Register />
+      <Login />
     </>
   );
 }
